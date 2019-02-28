083776d10f7e4f137666f675cded66a55ef63b61
jeferson <jefersonqueiroga@gmail.com>
2019-02-27 21:15:18 -0300

    Adicionando nova função

--- ProjetoEstudoGit/src/br/edu/ifrn/jeferson/Main.java
+++ ProjetoEstudoGit/src/br/edu/ifrn/jeferson/Main.java
@@ -3,5 +3,10 @@ package br.edu.ifrn.jeferson;
 public class Main {
 	public Main() {
 		System.out.println("Testando");
+		criandoNovaFuncaoImprimir();
+	}
+	
+	private void criandoNovaFuncaoImprimir() {
+		System.out.println("Testando");
 	}
 }

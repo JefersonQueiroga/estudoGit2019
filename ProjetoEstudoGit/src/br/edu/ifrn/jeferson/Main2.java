5d08c1399685a43320525e313abf5a6d9bcda768
jeferson <jefersonqueiroga@gmail.com>
2019-03-01 15:41:03 -0300

    Commite testando!

---
+++ ProjetoEstudoGit/src/br/edu/ifrn/jeferson/Main2.java
@@ -0,0 +1,7 @@
+package br.edu.ifrn.jeferson;
+
+public class Main2 {
+	public static void main(String[] args) {
+		System.out.println("Testando");
+	}
+}
---
+++ ProjetoEstudoGit/src/br/edu/ifrn/teste/InterfaceGrafica.java
@@ -0,0 +1,8 @@
+package br.edu.ifrn.teste;
+
+
+public class InterfaceGrafica {
+	public static void main(String[] args) {
+		System.out.println("Testando");
+	}
+}
